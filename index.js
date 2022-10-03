4584e7201f936cb268f3cd262ec50dc708e65ce0
Gabriel Thome <gabriel.alphon@gmail.com>
2022-10-03 17:26:12 -0300

    first commit

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
 require("dotenv").config();
-const { default: axios } = require("axios");
+const axios = require("axios");
+const crypto = require("crypto");
 const WebSocket = require("ws");
 
 const ws = new WebSocket(process.env.STREAM_URL + "btcusdt@bookTicker");
-
-//let isOpened = false;
+let isOpened = false;
 
 ws.onmessage = async (event) => {
     const obj = JSON.parse(event.data);
+    // console.log(obj);
     console.log("Symbol: " + obj.s);
     console.log("Price: " + obj.a);
 
-    /*
-    const price = parseFloat(obj.a);
-        // && !isOpened
-     if(price < 19430 ){
-        consolde.log("Buy");
-        //newOrder("BTCUSDT", "0.001", "BUY");
-        //isOpened = true;
-    }
-    // && isOpened
-    else if(price > 19440 ){
-        console.log("Sell");
-       //newOrder("BTCUSDT", "0.001", "SELL");
-        //isOpened = false;
-    } */
+    const price = parseFloat(obj.a); // string to float
     
-
+    if(price < 19560 && !isOpened){
+        console.log("Buy");
+        newOrder("BTCUSDT", "0.001", "BUY");
+        isOpened = true;
+    }else if(price > 19565 && isOpened){
+        console.log("Sell");
+        newOrder("BTCUSDT", "0.001", "SELL");
+        isOpened = false;
+    }
 }
 
-/*
 async function newOrder(symbol, quantity, side){
     const data = { symbol, quantity, side };
     data.type = "MARKET";
     data.timestamp = Date.now();
+
     const signature = crypto
         .createHmac("sha256", process.env.SECRET_KEY)
         .update(new URLSearchParams(data).toString())
@@ -45,6 +41,6 @@ async function newOrder(symbol, quantity, side){
         url: process.env.API_URL + "/v3/order?" + new URLSearchParams(data),
         headers: { "X-MBX-APIKEY": process.env.API_KEY } 
     })
+
     console.log(result.data);
 } 
-*/
